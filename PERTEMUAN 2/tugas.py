3f47d2d7bf3d2e46ec476f85faf21169fc45de70
gnafhan <nafhanghifari@gmail.com>
2023-08-28 12:00:00 +0700

    Fix

--- PERTEMUAN 2/tugas.py
+++ PERTEMUAN 2/tugas.py
@@ -1,8 +1,3 @@
-# baris = int(input("Masukkan jumlah baris: "))
-# for i in range(baris):
-#     print("* " * (i+1))
-
-
 baris = int(input("Masukkan baris: "))
 for i in range(baris):
     print("* " * baris)
